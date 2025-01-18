ca40ab6893b4b51abc55295b9b777138519c2bda
barbariansubhkaran <subhkaran.foam@gmail.com>
2025-01-18 11:43:17 +0530

    complete_rewrite

--- src/AMReX_headers.h
+++
@@ -1,7 +0,0 @@
-#include <AMReX_CArena.H>
-#include <AMReX_REAL.H>
-#include <AMReX_Amr.H>
-#include <AMReX_ParmParse.H>
-#include <AMReX_ParallelDescriptor.H>
-#include <AMReX_AmrLevel.H>
-#include <AMReX_EB2.H>
